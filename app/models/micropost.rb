26b7c5699f9ed5446ed750ba26ec7b46d88df787
Kohei Nishiwaki <128705560+hduehgw0@users.noreply.github.com>
2025-08-19 04:22:26 +0000

    Fix syntax error in Micropost model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: { maximum: 140 },
+  validates :content, length: { maximum: 140 }
                       ##「2.3.3の演習内容」
                       # presence: true
                       ## 「ここまで」
